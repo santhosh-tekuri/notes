87ef77c3e3b00804b8b2f5f7cf1f408ce20742e0
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-02-18 23:02:41 +0530

    Add instructions to install http-server

--- server.sh
+++ server.sh
@@ -1,3 +1,6 @@
+# ensure http-server is installed:
+#    npm install http-server -g
+
 rm -f nohup.out
-nohup http-server -c-1 &
+nohup http-server -a 127.0.0.1 -p 8080 -c-1 &
 echo open http://localhost:8080/index.html in browser
