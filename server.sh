9be39f3104532c336d8e5609854fcb9ec80105ee
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-07-01 13:10:02 +0530

    server.sh: generate cert if not present

--- server.sh
+++ server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
-openssl req -x509 -new -keyout key.pem -nodes -out cert.pem -subj '/C=IN/ST=Karnataka/O=MGMT/CN=www.abc.com'
+if [ \( ! -f cert.pem \) -o \( ! -f key.pem \) ]; then
+    openssl req -x509 -new -keyout key.pem -nodes -out cert.pem -subj '/C=IN/ST=Karnataka/O=MGMT/CN=www.abc.com'
+fi
 nohup openssl s_server -key key.pem -cert cert.pem -accept 8443 -WWW -quiet &
 echo open https://localhost:8443/index.html in browser
